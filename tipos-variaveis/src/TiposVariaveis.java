d8c14401761ce02fb0aaa57c5535648639a5cc9a
july-gusman <julianagusman95@gmail.com>
2024-05-21 10:30:46 -0300

    Aula sobre operadores do java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classes String que representa texto na aplicação
+
+        String meuNome = "Juliana Gusman"
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+        
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+
     }
 }
     
